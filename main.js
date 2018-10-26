9d24d0ddaab88e54f0a0b1cc98d00dd0835dc5fc
RosemarieLopezH <rosemarie61@gmail.com>
2018-10-26 12:51:10 -0500

    nav

--- main.js
+++ main.js
@@ -1,20 +1,38 @@
-const url = "https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&api_key=e941da3543824deaddb92310754b2a2d&format=json&limit=5&artist=brunomars"
-const artistSong ="https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&api_key=e941da3543824deaddb92310754b2a2d&format=json&limit=10&artist=brunomars"
+const container = document.getElementById('track-list');
 
-const traer =()=> {
-  fetch(url)
-    .then(res => res.json())
-    .then(datos => {
-      console.log(datos);
-    })
-}
-traer();
+const albumArtist = [
+  'BIGBANG',
+  'DUA LIPA',
+  'GOT7',
+  'BRUNO MARS',
+  'CARDI B',
+  'CAMILA CABELLO',
+  'BEYONCE',
+  'ED SHEERAN',
+  'POST MALONE'
+]
 
-const listar =()=> {
-  fetch(artistSong)
-    .then(res => res.json())
-    .then(datos => {
-      console.log(datos);
-    })
+let orderByLikeCount = (trackA, trackB) => trackB.likeCount - trackA.likeCount;
+const songs = [];
+const showArtist = () => {
+    fetch('http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=e941da3543824deaddb92310754b2a2d&format=json&limit=10&artist')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+};
+showArtist(); 
+
+const showSongs = ()=>{
+  const show = data.artists.artist;
+  show.map((artist)=>{
+    console.log(artist);
+    
+  })
+  container.innerHTML += 
+  `<div id="${showSongs}" class="col-6">${showSongs}</div>
+      <div class="col">
+      <div class="carousel-item active"></div>
+    </div>
+  `;
 }
-listar();
